70166237cdbcb6cc9aa9cc74dafe97ee29109264
Zeel B Patel <patel_zeel@iitgn.ac.in>
2021-12-10 12:16:36 +0530

    Update customize.py

--- customize.py
+++ customize.py
@@ -2,12 +2,10 @@ import os
 
 # Manually modify following parameters to customize the structure of your project
 path = os.path.abspath(os.path.dirname(__file__)).split("/")
-USERNAME = "patel-zeel"
+# print(path)
 REPO_HOME_PATH = "/".join(path[:-1])
 REPO_NAME = path[-1]
 PACKAGE_NAME = REPO_NAME
-BRANCH = "main"
-VERSION = "0.1.0"
 AUTHOR = "Zeel B Patel"
 AUTHOR_EMAIL = "patel_zeel@iitgn.ac.in"
 description = "example description"
@@ -19,21 +17,20 @@ LONG_DESCRIPTION_CONTENT_TYPE = "text/markdown"
 
 full_path = os.path.join(REPO_HOME_PATH, REPO_NAME)
 
-# Modify setup.cfg
+# Write setup.cfg
 
 with open(os.path.join(full_path, "setup.cfg"), "w") as f:
-    f.write(f"[metadata]\n")
-    f.write(f"name = {PACKAGE_NAME}\n")
-    f.write(f"version = {VERSION}\n")
-    f.write(f"author = {AUTHOR}\n")
-    f.write(f"author-email = {AUTHOR_EMAIL}\n")
-    f.write(f"description = {description}\n")
-    f.write(f"url = {URL}\n")
-    f.write(f"license = {LICENSE}\n")
-    f.write(f"long_description_content_type = {LONG_DESCRIPTION_CONTENT_TYPE}\n")
-    f.write(f"long_description = {LONG_DESCRIPTION}\n")
+    f.write("[metadata]\n")
+    f.write("name = " + PACKAGE_NAME + "\n")
+    f.write("author = " + AUTHOR + "\n")
+    f.write("author-email = " + AUTHOR_EMAIL + "\n")
+    f.write("description = " + description + "\n")
+    f.write("url = " + URL + "\n")
+    f.write("license = " + LICENSE + "\n")
+    f.write("long_description_content_type = " + LONG_DESCRIPTION_CONTENT_TYPE + "\n")
+    f.write("long_description = " + LONG_DESCRIPTION + "\n")
 
-# Modify CI
+# Write CI
 
 with open(os.path.join(full_path, ".github/workflows/CI.template"), "r") as f:
     content = f.read()
@@ -42,17 +39,27 @@ with open(os.path.join(full_path, ".github/workflows/CI.yml"), "w") as f:
     content = content.replace("<reponame>", REPO_NAME)
     f.write(content)
 
-# Modify README.md
-with open(os.path.join(full_path, "README.md"), "w") as f:
-    f.write(f"# {PACKAGE_NAME}\n")
-    f.write(
-        f"[![Coverage Status](https://coveralls.io/repos/github/{USERNAME}/{PACKAGE_NAME}/badge.svg?branch={BRANCH})](https://coveralls.io/github/{USERNAME}/{PACKAGE_NAME}/?branch={BRANCH}\n"
-    )
-    f.write(f"\n## Description\n")
-    f.write(f"* Run `customize.py` to customize this template.\n")
-    f.write(
-        f"* Add PYPI_USERNAME and PYPI_PASSWORD to your secrets using GitHub GUI.\n"
-    )
-    f.write(f"* Manually change requirements.txt to your needs.\n")
+# Write .gitignore
+with open(os.path.join(full_path, ".gitignore"), "w") as f:
+    f.write("__pycache__/\n")
+    f.write("*.vscode\n")
+    f.write("*.pyc\n")
+    f.write(f"{PACKAGE_NAME}/._version.py\n")
+
+
+# Write pyproject.toml
+with open(os.path.join(full_path, "pyproject.toml"), "w") as f:
+    f.write("[build-system]\n")
+    f.write("requires = [\n")
+    f.write('\t"setuptools>=50.0",\n')
+    f.write('\t"setuptools_scm[toml]>=6.0",\n')
+    f.write('\t"setuptools_scm_git_archive",\n')
+    f.write('\t"wheel>=0.33",\n')
+    f.write('\t"numpy>=1.16",\n')
+    f.write('\t"cython>=0.29",\n')
+    f.write("\t]\n")
+    f.write("\n")
+    f.write("[tool.setuptools_scm]\n")
+    f.write(f'write_to = "{PACKAGE_NAME}/_version.py"')
 
 print("Successful")
